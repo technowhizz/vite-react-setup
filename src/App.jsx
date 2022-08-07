033b3f119c8c88eb30d0dbd1bf557f7169f39d21
technowhizz <7688823+technowhizz@users.noreply.github.com>
2022-08-07 04:36:28 +0100

    Add public and components folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react"
+import Nav from "./components/Nav"
 
 export default function App(){
     return(
-        <h1>Test App</h1>
+        <div className="app">
+            <Nav />
+            <h1>Test App</h1>
+        </div>
     )
 }
